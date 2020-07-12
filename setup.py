827c732fbe7da892b344303697910da8ec7ccfbb
jchen42703 <jchen42703@gmail.com>
2020-07-11 21:57:09 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='interp',
+      version='0.00.1',
+      description='Library for model interpretability',
+      url='',
+      author='Joseph Chen, Benson Jin',
+      author_email='jchen42703@gmail.com',
+      license='Apache License Version 2.0, January 2004',
+      packages=['interp'],
+      install_requires=[
+            'numpy',
+            'torch',
+            'unittest'
+      ],
+      keywords=['deep learning', 'interpretability'],
+      )
